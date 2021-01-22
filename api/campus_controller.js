897e504906abc203b3055fb291d9318131bfb2bc
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-22 15:53:50 -0500

    cleanup campus controller

--- api/campus_controller.js
+++ api/campus_controller.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const models = require('../database/models');
+const express = require("express")
+const router = express.Router()
+const models = require("../database/models")
 
 // GET -> Read All
 // localhost:8080/api/campuses
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   models.Campus.findAll({
     // Eager Loading
     include: {
@@ -13,36 +13,36 @@ router.get('/', (req, res, next) => {
   })
     .then((campuses) => {
       res.status(200).json({
-        message: 'Got all campuses',
+        message: "Got all campuses",
         campuses,
-      });
+      })
     })
     .catch((err) => {
       res.status(500).json({
-        message: 'An error has occured getting all campuses',
+        message: "An error has occured getting all campuses",
         err,
-      });
-    });
-});
+      })
+    })
+})
 
 // GET by PK -> Read by ID
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   models.Campus.findByPk(req.params.id).then((campus) => {
     if (!campus) {
       res.status(404).json({
-        message: 'A campus by that id was not found to read',
-      });
+        message: "A campus by that id was not found to read",
+      })
     }
 
     res.status(200).json({
       campus,
-    });
-  });
-});
+    })
+  })
+})
 
 // POST
 // localhost:8080/api/campuses
-router.post('/', (req, res, send) => {
+router.post("/", (req, res, send) => {
   models.Campus.create({
     campusname: req.body.campusname,
     image: req.body.image,
@@ -51,25 +51,25 @@ router.post('/', (req, res, send) => {
   })
     .then((campus) => {
       res.status(200).json({
-        message: 'Successfully create campus!',
+        message: "Successfully create campus!",
         campus,
-      });
+      })
     })
     .catch((err) => {
       res.status(500).json({
-        message: 'An error has occured while creating campus',
+        message: "An error has occured while creating campus",
         err,
-      });
-    });
-});
+      })
+    })
+})
 
 // // PUT -> Update // insert below
-router.put('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   models.Campus.findByPk(req.params.id).then((campus) => {
     if (!campus) {
       res.status(404).json({
-        message: 'A campus by that id was not found to update',
-      });
+        message: "A campus by that id was not found to update",
+      })
     }
 
     campus.update({
@@ -77,36 +77,32 @@ router.put('/:id', (req, res, next) => {
       image: req.body.image,
       address: req.body.address,
       description: req.body.description,
-    });
+    })
 
-    campus.save();
+    campus.save()
 
     res.status(200).json({
-      message: 'Successfully updated campus',
+      message: "Successfully updated campus",
       campus,
-    });
-  });
-});
+    })
+  })
+})
 
 // DELETE -> Delete, timestamp:1:08:25
-router.delete('/:id', (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
   models.Campus.findByPk(req.params.id).then((campus) => {
     if (!campus) {
       res.status(404).json({
-        message: 'A campus by that id was not found to delete',
-      });
+        message: "A campus by that id was not found to delete",
+      })
     }
 
-    campus.destroy();
+    campus.destroy()
 
     res.status(200).json({
-      message: 'Successfully deleted campus',
-    });
-  });
-});
+      message: "Successfully deleted campus",
+    })
+  })
+})
 
-// PUT
-// localhost:8080/api/campuses/:id
-// router.put("/", (req, res, send))
-
-module.exports = router;
+module.exports = router
